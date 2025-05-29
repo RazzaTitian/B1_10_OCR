5e18fe6367870c685927a1995b4730468f7ab393
rhnathar <zildan.athar@gmail.com>
2025-05-30 03:09:58 +0700

    fix for deployment

--- clearscan/app/scan/page.tsx
+++ clearscan/app/scan/page.tsx
@@ -1,9 +1,9 @@
-'use client';
-import React from 'react';
-import { useScan } from './ScanContext';
-import ScanContent from '../../components/ScanContent';
+"use client";
+import React from "react";
+import { useScan } from "./ScanContext";
+import ScanContent from "../../components/ScanContent";
 
 export default function ScanPage() {
   const { selectedId } = useScan();
-  return <ScanContent selectedId={selectedId} />;
+  return <ScanContent selectedId={selectedId ?? ""} />;
 }
